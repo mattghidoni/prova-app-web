a94f3d034c0c5427ccc84ea2e8426d38f5958a6c
mattghidoni <ghidonim20@itisvinci.com>
2024-02-29 17:27:09 +0100

    creazione app

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+import { Suspense } from "react"
+import Loading from "./loading"
+
+export default function DashboardLayout({
+    children, // will be a page or nested layout
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <section>
+        {/* Include shared UI here e.g. a header or sidebar */}
+        <nav></nav>
+   
+        {children}
+      </section>
+    )
+  }
+
+  //<Suspense fallback={<Loading/>}></Suspense>
